b8c7007a591088bc67da955acbf54e2f7e94d1c9
davidhaas6 <davidhaas6@gmail.com>
2016-11-12 19:18:50 -0500

    cleaned up base template

--- TopicSearch.py
+++ TopicSearch.py
@@ -1,19 +1,22 @@
 import os
 from flask import render_template, flash
 import vocab_searcher
+import views
 from forms import SearchForm
 
 
 class TopicSearch:
-    def __init__(self, def_name, topic):
+    def __init__(self, def_name, subject, shorthand, description):
         def_path = os.path.join(os.path.dirname(__file__) + '/static/defs/', def_name) + '_defs.pickle'
         self.vocab = vocab_searcher.VocabSearch(def_path)
-        self.topic = topic
+        self.subject = subject
         self.def_name = def_name
+        self.shorthand = shorthand
+        self.description = description
 
     def search(self):
         form = SearchForm()
-        search_topic = "Search " + self.topic
+        search_topic = "Search " + self.subject
         if form.validate_on_submit():
             term = form.term.data
             print term
@@ -22,4 +25,5 @@ class TopicSearch:
                                title='Search a term',
                                form=form,
                                search_topic=search_topic,
-                               current={self.def_name: True})
+                               current={self.def_name: True},
+                               topics=views.topics)
--- views.py
+++ views.py
@@ -2,32 +2,49 @@ from flask import render_template
 from TopicSearch import TopicSearch
 from init import app
 
-gov = TopicSearch('gov', 'AP Government')
-enviro = TopicSearch('enviro', 'AP Environmental Sciences')
-psych = TopicSearch('psych', 'AP Psychology')
-bio = TopicSearch('bio', 'AP Biology')
-econ = TopicSearch('econ', 'AP Micro/macroeconomics')
-world = TopicSearch('world', 'AP World History')
-ushistory = TopicSearch('ushistory', 'AP US History')
-lang = TopicSearch('lang', 'AP English Language')
-euro = TopicSearch('euro', 'AP European History')
-humangeo = TopicSearch('humangeo', 'AP Human Geography')
+gov = TopicSearch(
+    def_name='gov', subject='AP Government', shorthand='AP Government', description='mr. dolan turnip')
+enviro = TopicSearch(
+    def_name='enviro', subject='AP Environmental Sciences', shorthand='AP Enviro', description='rip earth')
+psych = TopicSearch(
+    def_name='psych', subject='AP Psychology', shorthand='AP Psych', description='also a good tv show')
+bio = TopicSearch(
+    def_name='bio', subject='AP Biology', shorthand='AP Biology', description='biolology')
+econ = TopicSearch(
+    def_name='econ', subject='AP Micro/macroeconomics', shorthand='AP Micro/Macro',
+    description='grades lower than the peso')
+world = TopicSearch(
+    def_name='world', subject='AP World History', shorthand='AP World', description='i can show u the world')
+ushistory = TopicSearch(
+    def_name='ushistory', subject='AP US History', shorthand='AP US History', description='apush 2 far')
+lang = TopicSearch(
+    def_name='lang', subject='AP English Language', shorthand='AP Lang', description='lang gang')
+euro = TopicSearch(
+    def_name='euro', subject='AP European History', shorthand='AP Euro History', description='ur a\'peein')
+humangeo = TopicSearch(
+    def_name='humangeo', subject='AP Human Geography', shorthand='AP Human Geo', description='dis Ghana be good')
+
+topics = [world, enviro,
+          humangeo, bio,
+          gov, econ,
+          ushistory, psych,
+          euro, lang]
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html", current={})
+    return render_template("index.html", current={}, topics=topics)
 
 
 @app.route('/about')
 def about():
-    return render_template("about.html", current={})
+    return render_template("about.html", current={}, topics=topics)
 
 
 @app.route('/tips')
 def tips():
-    return render_template("tips.html", current={})
+    return render_template("tips.html", current={}, topics=topics)
 
 
 @app.route('/gov', methods=['GET', 'POST'])
